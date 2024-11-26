9c59c7ead1b8102210441285f30c32755adc4459
Your Name <you@example.com>
2024-11-26 13:09:53 +1100

    update

--- boot.py
+++ boot.py
@@ -5,11 +5,11 @@ import time
 ssid = "Aussie Broadband 6922"
 password = "xacskeyafa"
 
-#ssid = 'AndriodAP635'
-#password = 'Moh220582'
+#ssid = ''
+#password = ''
 
-#ssid = 'CyFi'
-#password = 'SecurityA40'
+#ssid = ''
+#password = ''
 
 station = network.WLAN(network.STA_IF)
 station.active(True)
@@ -23,8 +23,3 @@ print("Connected to Wi-Fi", station.ifconfig())
 
 
 # Supabase API configuration
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxenR2Zm9ka3h2dmZwcnlzbHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NTYxNTcsImV4cCI6MjA0NzAzMjE1N30.S1mFCvUvKGUSCkHeP-VmQiEcwQ-3hxbZ2fcf2cpgjDI"
-AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxenR2Zm9ka3h2dmZwcnlzbHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NTYxNTcsImV4cCI6MjA0NzAzMjE1N30.S1mFCvUvKGUSCkHeP-VmQiEcwQ-3hxbZ2fcf2cpgjDI"
-BASE_URL = "https://tqztvfodkxvvfpryslwc.supabase.co/rest/v1/Users"
-BASE_URL2 = "https://tqztvfodkxvvfpryslwc.supabase.co/rest/v1/TemperaturAndHumidity"
-BASE_URL3 = "https://tqztvfodkxvvfpryslwc.supabase.co/rest/v1/RfidScan"
